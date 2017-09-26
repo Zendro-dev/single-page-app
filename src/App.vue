4b88cc972b020f72c2ce0c36c52f40b8c363d80f
Dr. Asis Hallab <asis.hallab@gmail.com>
2017-09-26 13:37:50 +0000

    Added navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-    <h2>Router</h2>
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Reconstruct DB</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li><a href="#/users">Users</a></li>
+          <li><a href="#/taxons">Taxons</a></li>
+          <li><a href="#/cultivars">Cultivars</a></li>
+          <li><a href="#/individuals">Individuals</a></li>
+          <li><a href="#/microbiome_profiles">Microbiome-Profiles</a></li>
+          <li><a href="#/microbiome_samples">Microbiome-Samples</a></li>
+          <li><a href="#/pots">Pots</a></li>
+          <li><a href="#/soil_samples">Soil-Samples</a></li>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
