b2d8bc5ed38bb7c4bdaf5cff02e0954488a3df66
vsuaste <suaste@cimat.mx>
2018-12-14 02:59:55 -0600

    csv file size limit in globals

--- src/sciencedb-globals.js
+++ src/sciencedb-globals.js
@@ -5,6 +5,7 @@ module.exports.install = function(Vue, options) {
   Vue.prototype.$MAX_UPLOAD_SIZE = function(){
     return process.env.MAX_UPLOAD_SIZE || 500 // size in MB
   }
+
   Vue.prototype.$defaultDateFormat = function() {
     return "yy-mm-dd"
   }
