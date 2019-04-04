2302b47ea23639b3d734e5347305c22d4bcedf14
Dr. Asis Hallab <asis.hallab@gmail.com>
2019-04-04 23:17:34 +0200

    Using the vue-cli (v3) format for environment variables.

--- src/sciencedb-globals.js
+++ src/sciencedb-globals.js
@@ -1,14 +1,14 @@
 module.exports.install = function(Vue, options) {
   Vue.prototype.$baseUrl = function() {
-    return process.env.MY_SERVER_URL || "http://localhost:3000/graphql";
+    return process.env.VUE_APP_SERVER_URL || "http://localhost:3000/graphql";
   }
 
   Vue.prototype.$loginUrl = function() {
-    return process.env.MY_LOGIN_URL || "http://localhost:3000/login";
+    return process.env.VUE_APP_LOGIN_URL || "http://localhost:3000/login";
   }
 
   Vue.prototype.$MAX_UPLOAD_SIZE = function(){
-    return process.env.MAX_UPLOAD_SIZE || 500 // size in MB
+    return process.env.VUE_APP_MAX_UPLOAD_SIZE || 500 // size in MB
   }
   Vue.prototype.$defaultDateFormat = function() {
     return "yy-mm-dd"
