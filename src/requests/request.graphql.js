a7f977a97057f19cf8fd4de6047415f2da8cff11
Francisco Ramirez <franciscojrp07@gmail.com>
2020-02-03 01:30:45 -0600

    i#39 - [enhancement] - function makeCancelable() was moved to utils.js on spa-codegen

--- src/requests/request.graphql.js
+++ src/requests/request.graphql.js
@@ -10,39 +10,10 @@ export default function ({ url, query, variables }) {
       axios.defaults.headers.common['Authorization'] = 'Bearer ' + token;
     }
 
-    return makeCancelable(axios.post(url,
-        {
-            query: query,
-            variables: variables
-        }
-    ));
+    return axios.post(url,
+      {
+          query: query,
+          variables: variables
+      }
+    );
 }
-
-/**
- * makeCancelable()
- * 
- * This function equips a promise with a cancel() method.
- * 
- * Solution given by @istarkov on:  https://github.com/facebook/react/issues/5465#issuecomment-157888325
- * Also referenced here: https://reactjs.org/blog/2015/12/16/ismounted-antipattern.html
-*/
-
-const makeCancelable = (promise) => {
-  let hasCanceled_ = false;
-
-  const wrappedPromise = new Promise((resolve, reject) => {
-    promise.then((val) =>
-      hasCanceled_ ? reject({isCanceled: true}) : resolve(val)
-    );
-    promise.catch((error) =>
-      hasCanceled_ ? reject({isCanceled: true}) : reject(error)
-    );
-  });
-
-  return {
-    promise: wrappedPromise,
-    cancel() {
-      hasCanceled_ = true;
-    },
-  };
-};
