57afa2cb14e6b3bc85cabb327e869e63484e9195
Francisco Ramírez <franciscojrp07@gmail.com>
2020-09-27 09:16:54 -0500

    i61 - added config/globals.js - added new functions in utils

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 import decode from 'jwt-decode';
 import axios from 'axios';
+import getAttributes from './requests/requests.attributes'
 
 /**
  * makeCancelable()
@@ -127,4 +128,591 @@ export function requestGraphql({ url, query, variables }) {
         variables: variables
     }
   );
+}
+
+export function checkResponse(response, graphqlErrors, queryName) {
+  //check: internal errors
+  if(!Array.isArray(graphqlErrors)) throw new Error("internal_error: array expected");
+  if(typeof queryName !== 'string') throw new Error("internal_error: string expected");
+  
+  //check: server error
+  if(!response||!response.data) throw new Error("bad_server_response");
+  //check: graphql errors
+  if(response.data.errors) graphqlErrors.push({query: queryName, errors: response.data.errors});
+  //check: data errors
+  if(response.data.data === undefined) return "no_data"; 
+  if(response.data.data === null) return "null_data";
+  if(response.data.errors) return "with_errors";
+
+  return "ok";
+}
+
+/**
+ * Search utils
+ */
+export function getSearchArgument(filterName, searchText, ops, format, attributes) {
+  //internal checks
+  if(!getAttributes && !attributes) throw new Error("internal_error: attributes or getAttributes expected");
+  if(getAttributes && !filterName) throw new Error("internal_error: filterName expected");
+  
+  let filterAttributes = '';
+  if(attributes) filterAttributes = attributes;
+  else filterAttributes = getAttributes(filterName);
+  let modelAttributes = Object.keys(filterAttributes);
+  
+  //strings
+  let ors = '';
+  let orSearch = null;
+  let ands = '';
+  let andSearch = null;
+  //objects
+  let o_ors = [];
+  let o_orSearch = null;
+  let o_ands = [];
+  let o_andSearch = null;
+
+  if(searchText !== null && searchText !== '' && modelAttributes.length > 0) {
+    /*
+      Make AND fields
+    */
+    let words = searchText.split(' ');
+
+    //for each word
+    for(let w = 0; w < words.length; w++) {
+      /*
+        Make OR fields
+      */
+
+      //for each attribute
+      for(let i = 0; i < modelAttributes.length; i++) {
+        let num = 0;
+        let d = '';
+        let t = '';
+        let dt = '';
+
+        switch (filterAttributes[modelAttributes[i]]) {
+          case 'String':
+            //add
+            ors += `{field:${modelAttributes[i]}, value:"%${words[w]}%", operator:like},`;
+            o_ors.push({field: modelAttributes[i], value: `%${words[w]}%`, operator: "like"});
+            break;
+
+          case 'Int':
+            num = parseInt(words[w]);
+            //add if: word is an integer number
+            if (!isNaN(num)) {
+              ors += `{field:${modelAttributes[i]}, value:"${num}", operator:eq},`;
+              o_ors.push({field: modelAttributes[i], value: `${num}`, operator: "eq"});
+            }
+            break;
+
+          case 'Float':
+            num = parseFloat(words[w]);
+            //add if: word is a float number
+            if (!isNaN(num)) {
+              ors += `{field:${modelAttributes[i]}, value:"${num}", operator:eq},`;
+              o_ors.push({field: modelAttributes[i], value: `${num}`, operator: "eq"});
+            }
+            break;
+
+          case 'Boolean':
+            //add if: word is 'true' or 'false'
+            if (words[w] === 'true' || words[w] === 'false') {
+              ors += `{field:${modelAttributes[i]}, value:"${words[w]}", operator:eq},`;
+              o_ors.push({field: modelAttributes[i], value: `${words[w]}`, operator: "eq"});
+            }
+            break;
+
+          case 'Date':
+            d = getIsoDate(words[w]);
+            //add if: word is an ISO date
+            if (d !== '') {
+              ors += `{field:${modelAttributes[i]}, value:"${d}", operator:eq},`;
+              o_ors.push({field: modelAttributes[i], value: `${d}`, operator: "eq"});
+            }
+            break;
+
+          case 'Time':
+            t = getIsoTime(words[w]);
+            //add if: word is an ISO time
+            if (t !== '') {
+              ors += `{field:${modelAttributes[i]}, value:"${t}", operator:eq},`;
+              o_ors.push({field: modelAttributes[i], value: `${t}`, operator: "eq"});
+            }
+            break;
+
+          case 'DateTime':
+            dt = getIsoDateTime(words[w]);
+            //add if: word is an ISO datetime
+            if (dt !== '') {
+              ors += `{field:${modelAttributes[i]}, value:"${dt}", operator:eq},`;
+              o_ors.push({field: modelAttributes[i], value: `${dt}`, operator: "eq"});
+            }
+            break;
+
+          default:
+            break;
+        }
+
+        //make OR search argument
+        orSearch = `{operator:or, search: [ ${ors} ]},`;
+        o_orSearch = {operator: "or", search: o_ors};
+
+      }//end: for each attribute (ORs)
+
+      //add to ANDs
+      ands += orSearch;
+      o_ands.push(o_orSearch);
+
+    }//end: for each word (ANDs)
+
+    /*
+      Options
+    */
+    if (ops !== undefined && ops !== null && typeof ops === 'object') {
+
+      /*
+        -- 'only' option --
+        For each field name in only array, an AND search argument will be added to search string.
+
+        Format:
+          {
+            only: [
+              {
+                values: {
+                  'fieldName1': ['value1', 'value2', ..., 'valueN'],
+                  ...
+                  'fieldNameM': ['value1', 'value2', ..., 'valueN'],
+                }
+                type: 'type'
+              },
+              ...
+              {
+                values: {
+                  'fieldName1': ['value1', 'value2', ..., 'valueN'],
+                  ...
+                  'fieldNameN': ['value1', 'value2', ..., 'valueN'],
+                }
+                type: 'type'
+              }
+            ]
+          }
+      */
+      if (ops.hasOwnProperty('only') && Array.isArray(ops.only)) {
+        //strings
+        let onlyOrs = '';
+        let onlySearch = '';
+        //objects
+        let o_onlyOrs = [];
+        let o_onlySearch = {};
+
+
+        //for each only object
+        for(let i = 0; i < ops.only.length; i++) {
+          let o = ops.only[i];
+          /*
+            Switch type
+            At the momment, this only works for [InternalId] fields.
+            An internalID can be of types: Int, Float or String.
+          */
+          if (o.type === 'Int' || o.type === 'Float' || o.type === 'String') {
+            let v = o.values;
+            let vkeys = Object.keys(v);
+
+            //for each key
+            for(let k = 0; k < vkeys.length; k++) {
+              let va = v[vkeys[k]]; //values array
+
+              //in
+              onlyOrs += `{field:${vkeys[k]}, valueType: Array, value:"${va.join()}", operator:in},`;
+              o_onlyOrs.push({field: vkeys[k], valueType: "Array", value: va.join(), operator:"in"});
+
+            }//end: for earch key
+          }//end: if type 'Int
+        }//end: for earch only object
+
+        onlySearch = `{operator:or, search: [ ${onlyOrs} ]},`;
+        ands += onlySearch;
+
+        o_onlySearch = {operator: "or", search: o_onlyOrs};
+        o_ands.push(o_onlySearch);
+
+      }//end: if has 'only'
+
+      /*
+        -- 'exclude' option --
+        For each field name in exclude array, an AND search argument will be added to search string.
+
+        Format:
+          {
+            exclude: [
+              {
+                values: {
+                  'fieldName1': ['value1', 'value2', ..., 'valueN'],
+                  ...
+                  'fieldNameM': ['value1', 'value2', ..., 'valueN'],
+                }
+                type: 'type'
+              },
+              ...
+              {
+                values: {
+                  'fieldName1': ['value1', 'value2', ..., 'valueN'],
+                  ...
+                  'fieldNameN': ['value1', 'value2', ..., 'valueN'],
+                }
+                type: 'type'
+              }
+            ]
+          }
+      */
+      if (ops.hasOwnProperty('exclude') && Array.isArray(ops.exclude)) {
+        //for each exclude object
+        for(let i = 0; i < ops.exclude.length; i++) {
+          let o = ops.exclude[i];
+
+          /*
+            Switch type
+            At the momment, this only works for [InternalId] fields.
+            An internalID can be of types: Int, Float or String.
+          */
+          if (o.type === 'Int' || o.type === 'Float' || o.type === 'String') {
+            let v = o.values;
+            let vkeys = Object.keys(v);
+
+            //for each key
+            for(let k = 0; k < vkeys.length; k++) {
+              let va = v[vkeys[k]]; //values array
+
+              //notIn
+              ands += `{field:${vkeys[k]}, valueType: Array, value:"${va.join()}", operator:notIn},`;
+              o_ands.push({field: vkeys[k], valueType: "Array", value: va.join(), operator:"notIn"});
+
+            }//end: for earch key
+          }//end: if type 'Int
+        }//end: for earch exclude object
+      }//end: if has 'exclude'
+    }//end: if has 'ops'
+
+    //make search argument
+    andSearch = `search: {operator:and, search: [ ${ands} ]}`;
+    o_andSearch = {search: {operator: "and", search: o_ands}};
+
+    /**
+     * Debug
+     */
+    console.log("@@ andSearch: ", orSearch);
+    console.log("@@ o_andSearch: ", o_andSearch);
+  }//end: if searchText
+  else {
+    /*
+      Check: ops
+    */
+    /*
+      Options
+    */
+    if (ops !== undefined && ops !== null && typeof ops === 'object') {
+      /*
+        -- 'only' option --
+        For each field name in only array, an AND search argument will be added to search string.
+
+        Format:
+          {
+            only: [
+              {
+                values: {
+                  'fieldName1': ['value1', 'value2', ..., 'valueN'],
+                  ...
+                  'fieldNameM': ['value1', 'value2', ..., 'valueN'],
+                }
+                type: 'type'
+              },
+              ...
+              {
+                values: {
+                  'fieldName1': ['value1', 'value2', ..., 'valueN'],
+                  ...
+                  'fieldNameN': ['value1', 'value2', ..., 'valueN'],
+                }
+                type: 'type'
+              }
+            ]
+          }
+      */
+      if (ops.hasOwnProperty('only') && Array.isArray(ops.only)) {
+        //strings
+        let onlyOrs = '';
+        let onlySearch = '';
+        //objects
+        let o_onlyOrs = [];
+        let o_onlySearch = {};
+
+        //for each only object
+        for(let i = 0; i < ops.only.length; i++) {
+          let o = ops.only[i];
+          /*
+            Switch type
+            At the momment, this only works for [InternalId] fields.
+            An internalID can be of types: Int, Float or String.
+          */
+          if (o.type === 'Int' || o.type === 'Float' || o.type === 'String') {
+            let v = o.values;
+            let vkeys = Object.keys(v);
+
+            //for each key
+            for(let k = 0; k < vkeys.length; k++) {
+              let va = v[vkeys[k]]; //values array
+
+              //in
+              onlyOrs += `{field:${vkeys[k]}, valueType: Array, value:"${va.join()}", operator:in},`
+              o_onlyOrs.push({field: vkeys[k], valueType: "Array", value: va.join(), operator:"in"});
+
+            }//end: for earch key
+          }//end: if type 'Int
+        }//end: for earch only object
+
+        onlySearch = `{operator:or, search: [ ${onlyOrs} ]},`;
+        ands += onlySearch;
+
+        o_onlySearch = {operator: "or", search: o_onlyOrs};
+        o_ands.push(o_onlySearch);
+
+      }//end: if has 'only'
+
+      /*
+        -- 'exclude' option --
+        For each field name in exclude array, an AND search argument will be added to search string.
+
+        Format:
+          {
+            exclude: [
+              {
+                values: {
+                  'fieldName1': ['value1', 'value2', ..., 'valueN'],
+                  ...
+                  'fieldNameM': ['value1', 'value2', ..., 'valueN'],
+                }
+                type: 'type'
+              },
+              ...
+              {
+                values: {
+                  'fieldName1': ['value1', 'value2', ..., 'valueN'],
+                  ...
+                  'fieldNameN': ['value1', 'value2', ..., 'valueN'],
+                }
+                type: 'type'
+              }
+            ]
+          }
+      */
+      if (ops.hasOwnProperty('exclude') && Array.isArray(ops.exclude)) {
+
+        //for each exclude object
+        for(let i = 0; i < ops.exclude.length; i++) {
+          let o = ops.exclude[i];
+          /*
+            Switch type
+            At the momment, this only works for [InternalId] fields.
+            An internalID can be of types: Int, Float or String.
+          */
+          if (o.type === 'Int' || o.type === 'Float' || o.type === 'String') {
+            let v = o.values;
+            let vkeys = Object.keys(v);
+
+            //for each key
+            for(let k = 0; k < vkeys.length; k++) {
+              let va = v[vkeys[k]]; //values array
+
+              //notIn
+              ands += `{field:${vkeys[k]}, valueType: Array, value:"${va.join()}", operator:notIn},`
+              o_ands.push({field: vkeys[k], valueType: "Array", value: va.join(), operator:"notIn"});
+
+            }//end: for earch key
+          }//end: if type 'Int
+        }//end: for earch exclude object
+      }//end: if has 'exclude'
+
+      //make search argument
+      andSearch = `search: {operator:and, search: [ ${ands} ]}`
+      o_andSearch = {search: {operator: "and", search: o_ands}};
+
+      /**
+       * Debug
+       */
+      console.log("@@ andSearch: ", orSearch);
+      console.log("@@ o_andSearch: ", o_andSearch);
+    }//end: if has 'ops'
+  }//end: if !searchText
+
+  //return search filters
+  switch(format) {
+    case 'ands':
+      return ands;
+    case 'ands_object':
+      return o_ands;
+    case 'object':
+      return o_andSearch;
+    default: //string
+      return andSearch;
+  }
+}
+
+/**
+ * Local utils
+ *  
+ */
+function getIsoDate(text) {
+  //if has the form: aaaa[-/]mm[-/]dd
+  if (/^\d{4}[-/][01]\d[-/][0-3]\d/.test(text)) {
+
+    let m = text.slice(5, 7);
+    let d = text.slice(8, 10);
+
+    let numM = parseInt(m);
+    let numD = parseInt(d);
+
+    //if has the correct content
+    if ((numM >= 1 && numM <= 12) && (numD >= 1 && numD <= 31)) {
+      return text;
+    }
+  }
+  return '';
+}
+
+function getIsoTime(text) {
+
+  /**
+   * Case: complete precision: hh:mm:ss.d+
+   */
+  if (/^[0-2]\d:[0-5]\d:[0-5]\d\.\d+/.test(text)) {
+
+    let h = text.slice(0, 2);
+    let numH = parseInt(h);
+
+    if (numH >= 0 && numH <= 23) {
+      return text;
+    }
+
+    return '';
+  } else {
+    /**
+     * Case: no milliseconds: hh:mm:ss
+     */
+    if (/^[0-2]\d:[0-5]\d:[0-5]\d/.test(text)) {
+
+      let h = text.slice(0, 2);
+      let numH = parseInt(h);
+
+      if (numH >= 0 && numH <= 23) {
+        return text;
+      }
+
+      return '';
+    } else {
+      /**
+       * Case: no seconds: hh:mm
+       */
+      if (/^[0-2]\d:[0-5]\d/.test(text)) {
+
+        let h = text.slice(0, 2);
+        let numH = parseInt(h);
+
+        if (numH >= 0 && numH <= 23) {
+          return text;
+        }
+
+        return '';
+      }
+    }
+  }
+
+  return '';
+}
+
+function getIsoDateTime(text) {
+
+  /**
+   * Case: complete precision: YYYY[-/]MM[-/]DD[ T]hh:mm:ss.d+
+   */
+  if (/^\d{4}[/-][01]\d[/-][0-3]\d[T ][0-2]\d:[0-5]\d:[0-5]\d\.\d+/.test(text)) {
+
+    let M = text.slice(5, 7);
+    let D = text.slice(8, 10);
+    let h = text.slice(11, 13);
+
+    let numM = parseInt(M);
+    let numD = parseInt(D);
+    let numH = parseInt(h);
+
+    //if content ok
+    if ((numM >= 1 && numM <= 12) && (numD >= 1 && numD <= 31) && (numH >= 0 && numH <= 23)) {
+      return text;
+    }
+
+    return '';
+  } else {
+    /**
+     * Case: no milliseconds: YYYY[-/]MM[-/]DD[ T]hh:mm:ss
+     */
+    if (/^\d{4}[/-][01]\d[/-][0-3]\d[T ][0-2]\d:[0-5]\d:[0-5]\d/.test(text)) {
+
+      let M = text.slice(5, 7);
+      let D = text.slice(8, 10);
+      let h = text.slice(11, 13);
+
+      let numM = parseInt(M);
+      let numD = parseInt(D);
+      let numH = parseInt(h);
+
+      //if content ok
+      if ((numM >= 1 && numM <= 12) && (numD >= 1 && numD <= 31) && (numH >= 0 && numH <= 23)) {
+        return text;
+      }
+
+      return '';
+    } else {
+      /**
+       * Case: no seconds: YYYY[-/]MM[-/]DD[ T]hh:mm
+       */
+      if (/^\d{4}[/-][01]\d[/-][0-3]\d[T ][0-2]\d:[0-5]\d/.test(text)) {
+
+        let M = text.slice(5, 7);
+        let D = text.slice(8, 10);
+        let h = text.slice(11, 13);
+
+        let numM = parseInt(M);
+        let numD = parseInt(D);
+        let numH = parseInt(h);
+
+        //if content ok
+        if ((numM >= 1 && numM <= 12) && (numD >= 1 && numD <= 31) && (numH >= 0 && numH <= 23)) {
+          return text;
+        }
+
+        return '';
+      } else {
+        /**
+         * Case: no time: YYYY[-/]MM[-/]DD
+         */
+        if (/^\d{4}[/-][01]\d[/-][0-3]\d/.test(text)) {
+
+          let M = text.slice(5, 7);
+          let D = text.slice(8, 10);
+
+          let numM = parseInt(M);
+          let numD = parseInt(D);
+
+          //if content ok
+          if ((numM >= 1 && numM <= 12) && (numD >= 1 && numD <= 31)) {
+            return text;
+          }
+
+          return '';
+        }
+      }
+    }
+  }
+
+  return '';
 }
