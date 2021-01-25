038c860f251f42c9c5d5a96c3d3b3dd67af8947a
David Velasco <davvelsan@gmail.com>
2021-01-25 17:54:03 +0100

    chore: add eslint + prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["react-app", "plugin:react-hooks/recommended"],
+  rules: {},
+  overrides: [
+    {
+      files: ["**/*.ts?(x)"],
+      rules: {},
+    },
+  ],
+};
