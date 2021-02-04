d849619a504abdea7401bdd265f5f7affb667005
wunderbarr <wunderbardan@gmail.com>
2021-02-04 14:20:17 +0100

    refactor: add left icon & recreate style & fix value

--- src/components/input/StringField.jsx
+++ src/components/input/StringField.jsx
@@ -1,36 +1,56 @@
-import React, { useRef } from 'react';
-import { useTranslation } from 'react-i18next';
-import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+/**
+ * Use Cases:
+ * - component receives value => string | null
+ * - user types in the input => string (maybe empty)
+ * - user clicks on the reset button => null
+ *
+ * Notes: useRef might not be needed here, as long as value={value ?? ''} does
+ * not trigger an onChange event.
+ */
 
-export default function StringField({ value, ...props }) {
-  const { t } = useTranslation();
-  const computedValue = useRef(value);
-  const useStyles = makeStyles((theme) => ({
-    textField: {
-      margin: 'auto',
+import React, { useRef } from 'react';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      alignItems: 'start',
     },
-  }));
+    leftIcon: {
+      width: '2rem',
+      height: '2rem',
+      marginTop: theme.spacing(7),
+      marginRight: theme.spacing(2),
+      color: theme.palette.grey[700],
+    },
+  })
+);
+
+export default function StringField({ icon: Icon, value, ...props }) {
+  const computedValue = useRef(value);
+
   const classes = useStyles();
   const handleOnChange = (event) => {
-    if (!props.readOnly) {
-      computedValue.current = event.target.value;
+    computedValue.current = event.target.value;
 
-      if (props.onChange) {
-        props.onChange(props.label, computedValue.current);
-      }
+    if (props.onChange) {
+      props.onChange(props.label, computedValue.current);
     }
   };
 
   return (
-    <TextField
-      {...props}
-      fullWidth
-      className={classes.textField}
-      margin="normal"
-      variant="outlined"
-      value={value ? value : ''}
-      onChange={handleOnChange}
-    />
+    <div className={classes.root}>
+      {Icon && <Icon className={classes.leftIcon} />}
+      <TextField
+        {...props}
+        fullWidth
+        margin="normal"
+        variant="outlined"
+        value={value ?? ''}
+        onChange={handleOnChange}
+      />
+    </div>
   );
 }
