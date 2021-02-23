e822fb5a418b2f6c7da716e0106735671b50ffe8
David Velasco <davvelsan@gmail.com>
2021-02-23 16:33:43 +0100

    feat: add a default Zendro home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,75 +1,91 @@
+import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { NextPage } from 'next';
-import styles from '../styles/Home.module.css';
-import SiteLink from '../components/links/site-link';
+import Link from 'next/link';
 import useAuth from '@/hooks/useAuth';
+import {
+  AppBar,
+  Toolbar,
+  makeStyles,
+  createStyles,
+  Theme,
+  Button,
+} from '@material-ui/core';
+
+import LanguageSwitcher from '@/components/toolbar/lang-switcher';
 
 const Home: NextPage = () => {
+  const classes = useStyles();
   const { auth } = useAuth();
+  const [loginRoute, setLoginRoute] = useState('/login');
+
+  useEffect(() => {
+    if (!auth.user?.isValid) return;
+    setLoginRoute('/home');
+  }, [auth]);
 
   return (
-    <div className={styles.container}>
+    <div className={classes.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Zendro App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <AppBar position="fixed">
+        <Toolbar>
+          <div className={classes.appBarRightButtonsContainer}>
+            <LanguageSwitcher />
+            <Link href={loginRoute} passHref>
+              <Button className={classes.loginButton} component="a">
+                Login
+              </Button>
+            </Link>
+          </div>
+        </Toolbar>
+      </AppBar>
 
-        <SiteLink
-          color="primary"
-          href={auth.user?.isValid ? '/home' : '/login'}
-        >
-          Login
-        </SiteLink>
+      <main className={classes.main}>
+        <img className={classes.banner} src="/banner.png" alt="zendro banner" />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <div className={classes.cardContainer}>
+          <a
+            className={classes.card}
+            href="https://zendro-dev.github.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <p>Find in-depth information about Zendro features and API.</p>
           </a>
 
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            className={classes.card}
+            href="https://github.com/Zendro-dev"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+            <h3>Github &rarr;</h3>
+            <p>Contribute with pull requests, issues, and feedback</p>
           </a>
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className={classes.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          className={classes.footerLink}
+          href="https://nextjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <span>Powered by</span>
+          <img
+            style={{
+              paddingLeft: '.5rem',
+            }}
+            src="/nextjs.svg"
+            alt="Next Logo"
+            className={classes.logo}
+          />
         </a>
       </footer>
     </div>
@@ -77,3 +93,81 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '100vh',
+      padding: theme.spacing(0, 2),
+    },
+    appBarRightButtonsContainer: {
+      marginLeft: 'auto',
+    },
+    main: {
+      padding: theme.spacing(20, 0),
+      display: 'flex',
+      flexGrow: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    loginButton: {
+      color: 'white',
+    },
+    banner: {
+      padding: theme.spacing(8),
+      width: '100%',
+      objectFit: 'contain',
+    },
+    cardContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+      maxWidth: theme.breakpoints.width('md'),
+    },
+    card: {
+      padding: theme.spacing(8),
+      width: '100%',
+      border: '1px solid',
+      borderColor: theme.palette.grey[300],
+      transition: 'all',
+      transitionDuration: '.15s',
+      transitionTimingFunction: 'ease',
+      '&:hover': {
+        color: 'green',
+      },
+      [theme.breakpoints.down('sm')]: {
+        marginBottom: theme.spacing(2),
+      },
+      [theme.breakpoints.up('sm')]: {
+        flexBasis: '45%',
+        '&:nth-child(even)': {
+          marginLeft: theme.spacing(2),
+        },
+      },
+      [theme.breakpoints.up('md')]: {
+        margin: theme.spacing(2, 7),
+      },
+    },
+    logo: {
+      height: theme.spacing(6),
+    },
+    footer: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      height: theme.spacing(20),
+      borderTop: '1px solid',
+      borderColor: theme.palette.grey[300],
+    },
+    footerLink: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+  })
+);
