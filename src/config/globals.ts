79bd8b4def0456ddf359ed959c5ab68fab320576
David Velasco <davvelsan@gmail.com>
2021-02-24 14:14:30 +0100

    fix: enforce mandatory env variables

--- src/config/globals.ts
+++ src/config/globals.ts
@@ -1,19 +1,28 @@
 export const GRAPHQL_URL = process.env.NEXT_PUBLIC_ZENDRO_GRAPHQL_URL;
 export const LOGIN_URL = process.env.NEXT_PUBLIC_ZENDRO_LOGIN_URL;
 export const EXPORT_URL = process.env.NEXT_PUBLIC_ZENDRO_EXPORT_URL;
-export const MAX_UPLOAD_SIZE = Number(
-  process.env.NEXT_PUBLIC_ZENDRO_MAX_UPLOAD_SIZE
-); // size in MB
-export const MAX_RECORD_LIMIT = Number(
-  process.env.NEXT_PUBLIC_ZENDRO_MAX_RECORD_LIMIT
-);
+export const MAX_UPLOAD_SIZE = process.env.NEXT_PUBLIC_ZENDRO_MAX_UPLOAD_SIZE; // size in MB
+export const MAX_RECORD_LIMIT = process.env.NEXT_PUBLIC_ZENDRO_MAX_RECORD_LIMIT;
+export const REDUX_LOGGER = process.env.NEXT_PUBLIC_REDUX_LOGGER;
 
 const config = {
   GRAPHQL_URL,
   LOGIN_URL,
   EXPORT_URL,
-  MAX_UPLOAD_SIZE,
-  MAX_RECORD_LIMIT,
+  MAX_UPLOAD_SIZE: Number(MAX_UPLOAD_SIZE),
+  MAX_RECORD_LIMIT: Number(MAX_RECORD_LIMIT),
+  REDUX_LOGGER,
 };
+
+if (
+  !GRAPHQL_URL ||
+  !LOGIN_URL ||
+  !EXPORT_URL ||
+  !MAX_UPLOAD_SIZE ||
+  !MAX_RECORD_LIMIT
+) {
+  console.error(JSON.stringify(config, null, 2));
+  throw new Error('Some mandatory variables in `env.local` are not being set');
+}
 
 export default config;
