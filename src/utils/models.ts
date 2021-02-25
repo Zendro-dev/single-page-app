14600a6f528b6d329fae54acbbcd66b41ddc6ead
coeit <cbeiteneuer@gmail.com>
2021-02-25 16:27:18 +0100

    feat: models util add getForeignKeys + filter foreign keys option for getAttributeList

--- src/utils/models.ts
+++ src/utils/models.ts
@@ -1,17 +1,50 @@
 import {
-  DataModel,
+  Association,
   AttributeScalarType,
   AttributeArrayType,
-} from '../types/models';
+  DataModel,
+} from '@/types/models';
 
-interface TableColumn {
+/**
+ * Get all foreign keys for a given model. A foreign-key is understood as any attribute that
+ * is contained in any of the associated data models.
+ * @param dataModel the data model object to parse
+ */
+export function getForeignKeys(dataModel: DataModel): Set<string> {
+  const { attributes, associations, model } = dataModel;
+  const foreignKeys = new Set<string>();
+  /**
+   * Parser function that iterates over data model associations and returns foreign-key
+   * attributes that are not contained in the data model.
+   * @param keys a Set object containing foreign keys
+   * @param param1 each Association object
+   */
+  const parseAssociationsReducer = (
+    keys: Set<string>,
+    { keyIn, targetKey }: Association
+  ): Set<string> =>
+    attributes[targetKey] && keyIn !== model ? keys.add(targetKey) : keys;
+  if (associations) {
+    Object.values(associations).reduce(parseAssociationsReducer, foreignKeys);
+  }
+  return foreignKeys;
+}
+interface Attribute {
   name: string;
   type: AttributeScalarType | AttributeArrayType;
   readOnly: boolean;
 }
 
-export function getAttributeList(model: DataModel): Array<TableColumn> {
-  const tableColumns: Array<TableColumn> = Object.keys(model.attributes).map(
+interface GetAttributeListOptions {
+  excludeForeignKeys: boolean;
+}
+
+export function getAttributeList(
+  model: DataModel,
+  options?: GetAttributeListOptions
+): Array<Attribute> {
+  // Get an array of Attribute objects
+  const attributes: Array<Attribute> = Object.keys(model.attributes).map(
     (attribute) => {
       return {
         name: attribute,
@@ -21,18 +54,22 @@ export function getAttributeList(model: DataModel): Array<TableColumn> {
     }
   );
 
+  // Parse all attributes contained in associated models
+  if (options?.excludeForeignKeys) {
+    const foreignKeys = getForeignKeys(model);
+    attributes.filter(({ name }) => foreignKeys.has(name));
+  }
   model.internalId
-    ? tableColumns.splice(
+    ? attributes.splice(
         0,
         0,
-        tableColumns.splice(
-          tableColumns.findIndex((attr) => {
+        attributes.splice(
+          attributes.findIndex((attr) => {
             return attr.name === model.internalId;
           }),
           1
         )[0]
       )
-    : tableColumns.unshift({ name: 'id', type: 'Int', readOnly: true });
-
-  return tableColumns;
+    : attributes.unshift({ name: 'id', type: 'Int', readOnly: true });
+  return attributes;
 }
