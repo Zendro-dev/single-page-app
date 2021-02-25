414d97e5ad7bbaa26ca3919a382d20e1b309dad8
David Velasco <davvelsan@gmail.com>
2021-02-25 21:15:17 +0100

    feat: static record page (without queries)

--- src/pages/[model]/item.tsx
+++ src/pages/[model]/item.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import { useRouter } from 'next/router';
+
+import useAuth from '@/hooks/useAuth';
 import {
-  getStaticModels,
   getStaticModelPaths,
   getStaticRoutes,
+  getStaticModel,
 } from '@/utils/static';
+import { getAttributeList, Attribute } from '@/utils/models';
+import { PathParams } from '@/types/models';
 import { AppRoutes } from '@/types/routes';
-import { DataModels, PathParams } from '@/types/models';
-import useAuth from '@/hooks/useAuth';
+
 import ModelsLayout from '@/layouts/models-layout';
+import AttributesForm from '@/components/forms/attributes-form';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
 
 interface RecordProps {
-  dataModels: DataModels;
+  attributes: Attribute[];
+  modelName: string;
   routes: AppRoutes;
 }
 
@@ -24,29 +30,52 @@ export const getStaticPaths: GetStaticPaths<PathParams> = async () => {
   };
 };
 
-export const getStaticProps: GetStaticProps<RecordProps> = async () => {
+export const getStaticProps: GetStaticProps<RecordProps, PathParams> = async (
+  context
+) => {
+  const params = context.params as PathParams;
+
+  const modelName = params.model;
   const routes = await getStaticRoutes();
-  const dataModels = await getStaticModels();
+  const dataModel = await getStaticModel(modelName);
+
+  const attributes = getAttributeList(dataModel, { excludeForeignKeys: true });
 
   return {
     props: {
-      dataModels,
+      attributes,
+      modelName,
       routes,
     },
   };
 };
 
-const Record: NextPage<RecordProps> = ({ routes }) => {
+const Record: NextPage<RecordProps> = ({ attributes, modelName, routes }) => {
   useAuth({ redirectTo: '/' });
   const router = useRouter();
+  const { query } = router;
+  const classes = useStyles();
 
   return (
     <ModelsLayout brand="Zendro" routes={routes}>
-      <div>
-        <div>{JSON.stringify(router.asPath)} </div>
-        <div>{JSON.stringify(router.query)} </div>
-      </div>
+      <AttributesForm
+        title={modelName}
+        className={classes.form}
+        attributes={attributes}
+      />
     </ModelsLayout>
   );
 };
 export default Record;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    form: {
+      border: '2px solid',
+      borderRadius: 10,
+      borderColor: theme.palette.grey[300],
+      margin: theme.spacing(4),
+      padding: theme.spacing(10),
+    },
+  })
+);
