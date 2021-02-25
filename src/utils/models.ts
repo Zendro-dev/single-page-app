6e51b9ae3e8ed8b987cc822a241b2b1b11ee7164
coeit <cbeiteneuer@gmail.com>
2021-02-25 16:31:35 +0100

    initial: add utility modules

---
+++ src/utils/models.ts
@@ -0,0 +1,75 @@
+import {
+  Association,
+  AttributeScalarType,
+  AttributeArrayType,
+  DataModel,
+} from '@/types/models';
+
+/**
+ * Get all foreign keys for a given model. A foreign-key is understood as any attribute that
+ * is contained in any of the associated data models.
+ * @param dataModel the data model object to parse
+ */
+export function getForeignKeys(dataModel: DataModel): Set<string> {
+  const { attributes, associations, model } = dataModel;
+  const foreignKeys = new Set<string>();
+  /**
+   * Parser function that iterates over data model associations and returns foreign-key
+   * attributes that are not contained in the data model.
+   * @param keys a Set object containing foreign keys
+   * @param param1 each Association object
+   */
+  const parseAssociationsReducer = (
+    keys: Set<string>,
+    { keyIn, targetKey }: Association
+  ): Set<string> =>
+    attributes[targetKey] && keyIn !== model ? keys.add(targetKey) : keys;
+  if (associations) {
+    Object.values(associations).reduce(parseAssociationsReducer, foreignKeys);
+  }
+  return foreignKeys;
+}
+interface Attribute {
+  name: string;
+  type: AttributeScalarType | AttributeArrayType;
+  readOnly: boolean;
+}
+
+interface GetAttributeListOptions {
+  excludeForeignKeys: boolean;
+}
+
+export function getAttributeList(
+  model: DataModel,
+  options?: GetAttributeListOptions
+): Array<Attribute> {
+  // Get an array of Attribute objects
+  const attributes: Array<Attribute> = Object.keys(model.attributes).map(
+    (attribute) => {
+      return {
+        name: attribute,
+        type: model.attributes[attribute],
+        readOnly: model.internalId === attribute ? true : false,
+      };
+    }
+  );
+
+  // Parse all attributes contained in associated models
+  if (options?.excludeForeignKeys) {
+    const foreignKeys = getForeignKeys(model);
+    attributes.filter(({ name }) => foreignKeys.has(name));
+  }
+  model.internalId
+    ? attributes.splice(
+        0,
+        0,
+        attributes.splice(
+          attributes.findIndex((attr) => {
+            return attr.name === model.internalId;
+          }),
+          1
+        )[0]
+      )
+    : attributes.unshift({ name: 'id', type: 'Int', readOnly: true });
+  return attributes;
+}
---
+++ src/utils/requests.ts
@@ -0,0 +1,111 @@
+import { GRAPHQL_URL } from '@/config/globals';
+import axios, { AxiosResponse } from 'axios';
+import { getInflections } from '@/utils/inflection';
+import { AttributeScalarType } from '@/types/models';
+
+interface ServerResponse<D, E> {
+  data?: D | null;
+  errors?: Array<E>;
+  status: number;
+  statusText: string;
+}
+
+interface QueryVariables {
+  search: {
+    field: string;
+    value: string;
+    valueType: AttributeScalarType;
+  };
+  pagination: {
+    first: number;
+    last: number;
+    after: string;
+    before: string;
+    includeCursor: boolean;
+  };
+  order: {
+    field: string;
+    order: 'ASC' | 'DESC';
+  };
+}
+
+interface Query {
+  [key: string]: {
+    edges: QueryEdge[];
+  };
+}
+
+interface QueryEdge {
+  node: QueryNode;
+}
+
+// TODO type this correctly
+interface QueryNode {
+  [key: string]: any;
+}
+
+/**
+ * generic query interface to the backend graphql-server using axios
+ *
+ * @param query
+ * @param variables
+ * @param token
+ */
+export async function graphql<D, E = any>(
+  query: string,
+  variables: QueryVariables,
+  token: string
+): Promise<ServerResponse<D, E>> {
+  let response: AxiosResponse;
+  try {
+    response = await axios({
+      url: GRAPHQL_URL,
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+        Authorization: token ? `Bearer ${token}` : null,
+      },
+      data: {
+        query,
+        variables,
+      },
+    });
+  } catch (error) {
+    return {
+      errors: [error],
+      status: error.response.status,
+      statusText: error.response.statusText,
+    };
+  }
+
+  return {
+    data: response.data.data,
+    errors: response.data.errors,
+    status: response.status,
+    statusText: response.statusText,
+  };
+}
+
+export const readMany = <Q extends Query>(modelName: string) => async (
+  query: string,
+  variables: QueryVariables,
+  token: string
+): Promise<any> => {
+  const response = await graphql<Q>(query, variables, token);
+  const resData = response.data;
+  // ? useSWR hook returns this as {error}
+  const resErrors = response.errors;
+  if (resErrors) {
+    throw resErrors;
+  }
+
+  const resolver = `${getInflections(modelName).namePlLc}Connection` as keyof Q;
+
+  let data: QueryNode[] = [];
+
+  if (resData) {
+    data = resData[resolver].edges.map((edge) => edge.node);
+  }
+  return data;
+};
