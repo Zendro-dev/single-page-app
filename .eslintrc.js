072f0762b788a7ab73d6750c08bdf3589678fdce
David Velasco <davvelsan@gmail.com>
2021-02-25 21:15:33 +0100

    chore: allow underscore prefixed unused vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,7 +76,10 @@ module.exports = {
         'react/react-in-jsx-scope': 'off',
 
         // Warn about unused variables
-        '@typescript-eslint/no-unused-vars': ['warn'],
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          { argsIgnorePattern: '^_' },
+        ],
 
         // Require return types on functions only where useful
         '@typescript-eslint/explicit-function-return-type': [
