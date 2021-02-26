d40f43f6b852631f0d8847954b85d1633bf803cf
David Velasco <davvelsan@gmail.com>
2021-02-26 10:01:42 +0100

    chore: add default base props generic

--- src/types/elements.ts
+++ src/types/elements.ts
@@ -1,7 +1,7 @@
 import { InputProps, SvgIconTypeMap } from '@material-ui/core';
 import { OverridableComponent } from '@material-ui/core/OverridableComponent';
 
-export type BaseInputFieldProps<T> = T & {
+export type BaseInputFieldProps<T = Record<string, unknown>> = T & {
   error?: boolean;
   helperText?: string;
   InputProps?: InputProps;
