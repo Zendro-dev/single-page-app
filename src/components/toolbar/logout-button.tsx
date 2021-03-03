7891dbc14f916f33ac2d356d999f503690431033
vsuaste <suaste@cimat.mx>
2021-03-02 19:15:43 -0600

    internationalization added, language switcher working

--- src/components/toolbar/logout-button.tsx
+++ src/components/toolbar/logout-button.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from 'react';
 import useAuth from '../../hooks/useAuth';
 import { Button } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
 
 export default function LogoutButton(): ReactElement {
   const { logout } = useAuth();
+  const { t } = useTranslation();
 
   const handleLogoutButtonClick: React.MouseEventHandler<HTMLButtonElement> = () => {
     logout();
@@ -11,7 +13,7 @@ export default function LogoutButton(): ReactElement {
 
   return (
     <Button color="inherit" onClick={handleLogoutButtonClick}>
-      Logout
+      {t('toolBar.logout')}
     </Button>
   );
 }
