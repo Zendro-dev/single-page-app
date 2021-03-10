9654b8a08b5d1f37831661b643c5c061847434f8
David Velasco <davvelsan@gmail.com>
2021-03-10 15:04:17 +0100

    feat: add redirectTimer API to useAuth

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 import { authSelector, logUserIn, logUserOut } from '../store/auth-slice';
@@ -8,6 +8,7 @@ interface AuthOptions {
   redirectTo: string;
   redirectIfFound?: boolean;
   redirectIfNotAllowed?: boolean;
+  redirectTimeout?: number;
 }
 type AuthLogin = (email: string, password: string) => void;
 type AuthLogout = () => void;
@@ -16,13 +17,18 @@ interface UseAuth {
   auth: AuthState;
   login: AuthLogin;
   logout: AuthLogout;
+  isAllowed: () => boolean | undefined;
+  redirectTimer: number;
 }
 
 export default function useAuth(options?: AuthOptions): UseAuth {
-  const { redirectTo, redirectIfFound, redirectIfNotAllowed } = options ?? {};
+  const { redirectTo, redirectIfFound, redirectIfNotAllowed, redirectTimeout } =
+    options ?? {};
   const auth = useSelector(authSelector);
   const dispatch = useDispatch();
   const router = useRouter();
+
+  const [redirectTimer, setRedirectTimer] = useState(redirectTimeout ?? 0);
 
   const isAllowed = useCallback(() => {
     const model = router.query.model as string;
@@ -36,41 +42,56 @@ export default function useAuth(options?: AuthOptions): UseAuth {
     );
   }, [auth.user, router.query]);
 
-  useEffect(() => {
-    /**
-     * Do not do anything if:
-     * - No redirect needed
-     * - User data not set because there is a fetch in progress
-     */
-    if (!redirectTo || auth.status === 'loading') return;
+  useEffect(function updateTimer() {
+    const id = setInterval(
+      () => setRedirectTimer((count) => (count === 0 ? count : count - 1)),
+      1000
+    );
+    return () => clearInterval(id);
+  }, []);
 
-    if (
+  useEffect(
+    function redirectUser() {
       /**
-       * TOP PRECEDENCE: Redirect if only redirectTo is set and
-       * a valid user was not found
+       * Do not do anything if:
+       * - No redirect needed
+       * - User data not set because there is a fetch in progress
        */
-      (redirectTo && !redirectIfFound && !auth.user) ||
-      /**
-       * MID PRECEDENCE: Redirect if redirectIfFound is also set,
-       * and a valid user was found
-       */
-      (redirectIfFound && auth.user) ||
-      /**
-       * BOTTOM PRECEDENCE: Redirect if redirectIfNotAllowed is also
-       * set and the user does not have sufficient permissions
-       */
-      (redirectIfNotAllowed && auth.user && !isAllowed())
-    ) {
-      router.push(redirectTo);
-    }
-  }, [
-    auth,
-    isAllowed,
-    redirectTo,
-    redirectIfFound,
-    redirectIfNotAllowed,
-    router,
-  ]);
+      if (!redirectTo || auth.status === 'loading') return;
+
+      if (
+        /**
+         * TOP PRECEDENCE: Redirect if only redirectTo is set and
+         * a valid user was not found
+         */
+        (redirectTo && !redirectIfFound && !auth.user) ||
+        /**
+         * MID PRECEDENCE: Redirect if redirectIfFound is also set,
+         * and a valid user was found
+         */
+        (redirectIfFound && auth.user) ||
+        /**
+         * BOTTOM PRECEDENCE: Redirect if redirectIfNotAllowed is also
+         * set and the user does not have sufficient permissions
+         */
+        (redirectIfNotAllowed && auth.user && !isAllowed())
+      ) {
+        setTimeout(
+          () => router.push(redirectTo),
+          redirectTimeout ? redirectTimeout * 1000 : 0
+        );
+      }
+    },
+    [
+      auth,
+      isAllowed,
+      redirectTo,
+      redirectIfFound,
+      redirectIfNotAllowed,
+      redirectTimeout,
+      router,
+    ]
+  );
 
   const login: AuthLogin = (email, password) => {
     if (email && password) dispatch(logUserIn({ email, password }));
@@ -80,5 +101,5 @@ export default function useAuth(options?: AuthOptions): UseAuth {
     dispatch(logUserOut());
   };
 
-  return { auth, login, logout };
+  return { auth, login, logout, isAllowed, redirectTimer };
 }
