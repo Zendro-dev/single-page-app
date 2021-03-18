efd8585102dd4929261df6ab8c4610d6b7633867
vsuaste <suaste@cimat.mx>
2021-03-18 01:44:49 -0600

    delete unused code

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -5,36 +5,6 @@ type ValidationError = {
   message: string;
 };
 
-export function parseValidationErrors(errors: any): Record<string, string[]> {
-  const result: Record<string, string[]> = {};
-
-  errors.forEach((error: any) => {
-    if (error.response && error.response.data && error.response.data.errors) {
-      const errors = error.response.data.errors;
-      const validation_error = errors.find(
-        (e: any) => e.message === 'validation failed'
-      );
-      if (validation_error) {
-        const errors_detail: ValidationError[] =
-          validation_error.extensions.validationErrors;
-        errors_detail.forEach(({ dataPath, message }) => {
-          const attribute = dataPath.slice(1);
-          result[attribute]
-            ? result[attribute].push(message)
-            : (result[attribute] = [message]);
-        });
-      }
-    }
-  });
-
-  //unique error
-  for (const [key, errors] of Object.entries(result)) {
-    result[key] = Array.from(new Set(errors));
-  }
-
-  return result;
-}
-
 function getValidationErrors(errors:any) {
   let result:Record<string, string[]> = {};
 
