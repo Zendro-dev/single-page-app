f541a6a6af13c0a8dc74421a70b4451359f445a6
Veronica Suaste <suaste@cimat.mx>
2021-03-19 02:30:01 -0600

    feat: Error handling record page (#50)
    * validation errors in create form
    
    * validation errors as array
    
    * helperText as component, setup for handling client side validation
    
    * client side validation for int
    
    * client validation: float, int onKeyDown event
    
    * fix uniqueness in error array
    
    * refactor: adapt requestOne to use the new graphqlRequest function
    
    * handle general errors in record page
    
    * delete unused code
    
    * Typing error methods
    
    * chore: remove custom files
    
    Co-authored-by: David Velasco <davvelsan@gmail.com>
    Co-authored-by: coeit <cbeiteneuer@gmail.com>

--- src/components/input/float-field.tsx
+++ src/components/input/float-field.tsx
@@ -3,11 +3,13 @@ import BaseField, { BaseFieldProps } from './base-field';
 
 export interface FloatFieldProps {
   onChange?: (value: number | null) => void;
+  onError?: (value: string | null) => void;
   value: number | null;
 }
 
 export default function FloatField({
   onChange,
+  onError,
   value,
   ...props
 }: BaseFieldProps<FloatFieldProps>): ReactElement {
@@ -17,8 +19,22 @@ export default function FloatField({
     const floatValue =
       event.target.value === '' ? null : parseFloat(event.target.value);
 
-    if ((floatValue === null || !isNaN(floatValue)) && onChange) {
+    if ((floatValue === null || !isNaN(floatValue)) && onChange && onError) {
+      onError(null);
       onChange(floatValue);
+    }
+  };
+
+  const handleOnKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    if (/^[+\-.e]{1}$/.test(event.key)) {
+      return;
+    }
+    if (/^\D{1}$/.test(event.key)) {
+      if (onError) onError('Please enter a valid number');
+      event.preventDefault();
+      return;
     }
   };
 
@@ -26,6 +42,7 @@ export default function FloatField({
     <BaseField
       {...props}
       onChange={handleOnChange}
+      onKeyDown={handleOnKeyDown}
       type="number"
       value={value ?? ''}
     />
