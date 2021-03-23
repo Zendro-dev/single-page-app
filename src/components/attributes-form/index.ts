53303daed1ec3bddf184c96a32a0869b5ac860d9
David Velasco <davvelsan@gmail.com>
2021-03-23 15:45:16 +0100

    refactor: cleanup unused modules - form-actions component - error utils

--- src/components/attributes-form/index.ts
+++ src/components/attributes-form/index.ts
@@ -1,10 +1,9 @@
 import AttributesForm from './form';
-import FormActions from './form-actions';
 import FormHeader from './form-header';
 
 export * from './form';
 export * from './form-header';
 export * from './form-utils';
 
-export { FormActions, FormHeader };
+export { FormHeader };
 export default AttributesForm;
