c3dda5d9dc32f0f29d8d50cb12dc8f95061a22c7
David Velasco <davvelsan@gmail.com>
2021-03-23 15:34:10 +0100

    chore: add index module to hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,7 @@
+import useAuth from './useAuth';
+import useCoundown from './useCountdown';
+import { useDialog } from './useDialog';
+import useRedirect from './useRedirect';
+import useToastNotification from './useToastNotification';
+
+export { useAuth, useCoundown, useDialog, useRedirect, useToastNotification };
