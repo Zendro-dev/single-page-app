b9201c56c450bd0751d0d1942c596fe9de3bdae6
David Velasco <davvelsan@gmail.com>
2021-03-24 17:10:08 +0100

    refactor: remove unused requests utils

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
-import { request } from 'graphql-request';
-import { Variables } from 'graphql-request/dist/types';
 import { GRAPHQL_URL } from '@/config/globals';
 import { QueryVariables } from '@/types/queries';
-import { GraphQLErrors, RequestOneResponse } from '@/types/requests';
+import { GraphQLErrors } from '@/types/requests';
 
 export interface GraphqlResponse<T = unknown> {
   data?: T | null;
@@ -50,64 +48,6 @@ export async function graphqlRequest<T = unknown>(
 
   // ? check for response.data ?
   return { data: response.data.data, errors: response.data.errors };
-}
-
-interface GraphqlRequestOptions {
-  resolver?: string;
-  variables?: Variables;
-}
-
-export async function graphqlRequest2<T = unknown>(
-  token: string,
-  query: string,
-  options?: {
-    variables?: Variables;
-  }
-): Promise<Record<string, T>>;
-
-export async function graphqlRequest2<T = unknown>(
-  token: string,
-  query: string,
-  options: {
-    resolver: string;
-    variables?: Variables;
-  }
-): Promise<T>;
-
-export async function graphqlRequest2<T = unknown>(
-  token: string,
-  query: string,
-  options?: GraphqlRequestOptions
-): Promise<T | Record<string, T>> {
-  let res: T | Record<string, T>;
-
-  res = await request<Record<string, T>>(
-    GRAPHQL_URL as string,
-    query,
-    options?.variables
-  );
-
-  if (options?.resolver) res = res[options?.resolver];
-  return res;
-}
-
-export async function requestOne<T>(
-  token: string,
-  query: string,
-  resolver: string,
-  variables: QueryVariables,
-  additionalData?: { [key: string]: unknown }
-): Promise<GraphqlResponse<T>> {
-  const { data, errors } = await graphqlRequest<RequestOneResponse<T>>(
-    token,
-    query,
-    variables,
-    additionalData
-  ).catch((err) => {
-    throw err;
-  });
-
-  return { data: data ? data[resolver] : data, errors };
 }
 
 /**
