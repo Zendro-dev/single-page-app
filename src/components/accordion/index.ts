0697480a663c50ed37eb5898c042e1c1ad394ae2
David Velasco <davvelsan@gmail.com>
2021-03-29 12:44:10 +0200

    refactor: generalize accordion component

--- src/components/accordion/index.ts
+++
@@ -1,8 +0,0 @@
-import Accordion from './collapse';
-import AccordionItem from './item';
-
-export * from './collapse';
-export * from './item';
-
-export { AccordionItem };
-export default Accordion;
