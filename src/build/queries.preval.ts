f72df7e47943edb399a2aea8dc697d0aa20b61da
David Velasco <davvelsan@gmail.com>
2021-04-01 10:19:55 +0200

    refactor: do not create gql files (yet)

--- src/build/queries.preval.ts
+++ src/build/queries.preval.ts
@@ -1,23 +1,23 @@
-import { mkdir, writeFile } from 'fs/promises';
+// import { mkdir, writeFile } from 'fs/promises';
 import preval from 'next-plugin-preval';
-import { join } from 'path';
-import { format } from 'prettier';
+// import { join } from 'path';
+// import { format } from 'prettier';
 import { getStaticQueries } from '@/utils/static';
 import { StaticQueries } from '@/types/static';
 
 async function buildQueries(): Promise<Record<string, StaticQueries>> {
   const modelQueries = await getStaticQueries();
 
-  for (const [model, queries] of Object.entries(modelQueries)) {
-    const modelDir = `src/custom/queries/${model}`;
-    await mkdir(modelDir, { recursive: true });
-    for (const [queryName, { query }] of Object.entries(queries)) {
-      await writeFile(
-        join(modelDir, queryName) + '.gql',
-        format(query, { parser: 'graphql' })
-      );
-    }
-  }
+  // for (const [model, queries] of Object.entries(modelQueries)) {
+  //   const modelDir = `src/custom/queries/${model}`;
+  //   await mkdir(modelDir, { recursive: true });
+  //   for (const [queryName, { query }] of Object.entries(queries)) {
+  //     await writeFile(
+  //       join(modelDir, queryName) + '.gql',
+  //       format(query, { parser: 'graphql' })
+  //     );
+  //   }
+  // }
 
   return modelQueries;
 }
