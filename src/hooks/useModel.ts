3587f536985ed8db05dd4200424e14df5aa9368b
David Velasco <davvelsan@gmail.com>
2021-04-14 22:03:55 +0200

    refactor: remove unused imports

--- src/hooks/useModel.ts
+++ src/hooks/useModel.ts
@@ -1,9 +1,9 @@
+import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import dataModels from '@/build/models.preval';
 import { authSelector } from '@/store/auth-slice';
 import { ParsedDataModel2 } from '@/types/models';
-import { AclPermission, ParsedPermissions } from '@/types/acl';
-import { useCallback, useMemo } from 'react';
+import { ParsedPermissions } from '@/types/acl';
 
 interface UseModel {
   permissions: ParsedPermissions;
