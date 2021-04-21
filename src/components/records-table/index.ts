88e0caf9f5bfef35bc84288f6a58a5e7ffa1194d
coeit <cbeiteneuer@gmail.com>
2021-04-21 16:19:39 +0200

    chore: remove deprecated records-table folder

--- src/components/records-table/index.ts
+++
@@ -1,9 +0,0 @@
-import Table from './table2';
-import TablePagination from './table-pagination';
-import TableToolBar from './table-toolbar';
-import useVariables from './useVariables';
-
-export * from './table2';
-export * from './table-row';
-
-export { Table, TableToolBar, TablePagination, useVariables };
