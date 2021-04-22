2d3814dcf49936563b81a75e4275108f3223b0c6
vsuaste <suaste@cimat.mx>
2021-04-21 20:12:07 -0500

    update array field: accordion

--- src/components/input/array-field.tsx
+++ src/components/input/array-field.tsx
@@ -1,14 +1,9 @@
 import { ReactElement } from 'react';
 import { Overwrite } from 'utility-types';
 
-import ListSubheader from '@material-ui/core/ListSubheader';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import AddIcon from '@material-ui/icons/Add';
 
-import { InputAdornment, SvgIconProps } from '@material-ui/core';
+import { Button, InputAdornment, SvgIconProps } from '@material-ui/core';
 import { Clear as ClearIcon } from '@material-ui/icons';
 import { Delete as DeleteIcon } from '@material-ui/icons';
 
@@ -23,6 +18,11 @@ import {
 } from '@/components/input';
 
 import ClickableIcon from '@/components/buttons/icon-button';
+
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import { AttributeArrayValue, AttributeScalarValue } from '@/types/models';
 
@@ -90,7 +90,10 @@ export default function ArrayField({
     }
   };
 
-  const addItem = (index: number): void => {
+  const addItem = (index: number) => (
+    event: React.MouseEvent<HTMLButtonElement>
+  ): void => {
+    event.preventDefault();
     value ? value.splice(index + 1, 0, null) : (value = [null]);
     if (onChange) {
       onChange(value);
@@ -112,66 +115,72 @@ export default function ArrayField({
   };
 
   return (
-    <List subheader={<ListSubheader> {label} </ListSubheader>}>
-      {onChange && (
-        <ListItem
-          button
-          onClick={() => {
-            addItem(-1);
-          }}
-        >
-          <ListItemIcon>
-            <AddIcon />
-          </ListItemIcon>
-          <ListItemText primary="Add item" />
-        </ListItem>
-      )}
-      {value &&
-        value.map((v: AttributeScalarValue, index: number) => (
-          <ListItem key={index}>
-            <FieldIcons
-              rightIcon={
-                onChange
-                  ? (props: SvgIconProps): ReactElement => (
-                      <ClickableIcon
-                        tooltip="Add item below"
-                        handleOnClick={() => addItem(index)}
-                      >
-                        <AddIcon {...props} />
-                      </ClickableIcon>
-                    )
-                  : undefined
-              }
-            >
-              <ArrayInputField
-                {...props}
-                type={type}
-                value={v}
-                label={'array item'}
-                onChange={handleOnChange(index)}
-                onError={onError}
-                endAdornment={
-                  onChange && (
-                    <InputAdornment position="end">
-                      <ClickableIcon
-                        tooltip="Unset item"
-                        handleOnClick={() => handleOnClear(index)}
-                      >
-                        <ClearIcon />
-                      </ClickableIcon>
-                      <ClickableIcon
-                        tooltip="Delete item"
-                        handleOnClick={() => deleteItem(index)}
-                      >
-                        <DeleteIcon />
-                      </ClickableIcon>
-                    </InputAdornment>
-                  )
-                }
-              />
-            </FieldIcons>
-          </ListItem>
-        ))}
-    </List>
+    <div style={{ width: '100%', paddingBottom: '10px' }}>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          {label}
+        </AccordionSummary>
+        <AccordionDetails>
+          <fieldset
+            style={{
+              width: '100%',
+              height: '150px',
+              overflow: 'auto',
+            }}
+          >
+            {onChange && (
+              <Button fullWidth={true} onClick={addItem(-1)}>
+                <AddIcon /> Add item
+              </Button>
+            )}
+            {value &&
+              value.map((v: AttributeScalarValue, index: number) => (
+                <div key={index}>
+                  <FieldIcons
+                    rightIcon={
+                      onChange
+                        ? (props: SvgIconProps): ReactElement => (
+                            <ClickableIcon
+                              tooltip="Delete item"
+                              handleOnClick={() => deleteItem(index)}
+                            >
+                              <DeleteIcon {...props} />
+                            </ClickableIcon>
+                          )
+                        : undefined
+                    }
+                  >
+                    <ArrayInputField
+                      {...props}
+                      type={type}
+                      value={v}
+                      label={'item ' + index}
+                      onChange={handleOnChange(index)}
+                      onError={onError}
+                      endAdornment={
+                        onChange && (
+                          <InputAdornment position="end">
+                            <ClickableIcon
+                              tooltip="Unset item"
+                              handleOnClick={() => handleOnClear(index)}
+                            >
+                              <ClearIcon />
+                            </ClickableIcon>
+                          </InputAdornment>
+                        )
+                      }
+                    />
+                  </FieldIcons>
+                  {onChange && (
+                    <Button fullWidth={true} onClick={addItem(index)}>
+                      <AddIcon /> Add item
+                    </Button>
+                  )}
+                </div>
+              ))}
+          </fieldset>
+        </AccordionDetails>
+      </Accordion>
+    </div>
   );
 }
