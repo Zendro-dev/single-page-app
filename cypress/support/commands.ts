082d39d7c84a4fc121eaad2156c31cc3853f7be1
coeit <cbeiteneuer@gmail.com>
2021-05-26 10:00:13 +0200

    fix: remove no longer used import

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,3 @@
-import 'cypress-localstorage-commands';
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
