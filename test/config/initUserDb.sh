abbde38068a09d408350d2b27a7a3dae3bf15a63
David Velasco <davvelsan@gmail.com>
2021-06-22 11:16:25 +0200

    tests: update tests to work with a cassandra setup

--- test/config/initUserDb.sh
+++ test/config/initUserDb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	CREATE USER sciencedb WITH SUPERUSER PASSWORD 'sciencedb';
-	CREATE DATABASE sciencedb_development OWNER sciencedb;
-	CREATE DATABASE sciencedb_test OWNER sciencedb;
-	CREATE DATABASE sciencedb_production OWNER sciencedb;
+	CREATE USER zendro WITH SUPERUSER PASSWORD 'zendro';
+	CREATE DATABASE zendro_development OWNER zendro;
+	CREATE DATABASE zendro_test OWNER zendro;
+	CREATE DATABASE zendro_production OWNER zendro;
 EOSQL
