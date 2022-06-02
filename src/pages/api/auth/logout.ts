3892b06c690d0128d22c76eff6404093e1cc31e9
coeit <cbeiteneuer@gmail.com>
2022-06-02 15:37:47 +0200

    fix: logout url redirect_uri

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -2,11 +2,12 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 const OAUTH2_ISSUER = String(process.env.OAUTH2_ISSUER ?? '');
 const NEXTAUTH_URL = String(process.env.NEXTAUTH_URL ?? '');
+const BASE_URL = NEXTAUTH_URL.replace('/api/auth', '');
 
 export default (req: NextApiRequest, res: NextApiResponse): void => {
   res.redirect(
     `${OAUTH2_ISSUER}/protocol/openid-connect/logout?redirect_uri=${encodeURIComponent(
-      NEXTAUTH_URL
+      BASE_URL
     )}`
   );
 };
